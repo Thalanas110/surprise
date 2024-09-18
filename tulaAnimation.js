a8e93ad5e5ea221306d917fed3d0811972f8d88c
Thalanas110 <120995590+Thalanas110@users.noreply.github.com>
2024-09-19 05:18:11 +0800

    Added close dropdown when clicking outside

--- tulaAnimation.js
+++ tulaAnimation.js
@@ -1,10 +1,8 @@
-// Toggle dropdown visibility
 document.querySelector('.custom-dropdown-btn').addEventListener('click', function() 
 {
     this.parentElement.classList.toggle('show');
 });
 
-    // Close dropdown if clicked outside
 window.onclick = function(event) 
 {
     if (!event.target.matches('.custom-dropdown-btn')) 
@@ -13,9 +11,9 @@ window.onclick = function(event)
         for (var i = 0; i < dropdowns.length; i++) 
         {
             var openDropdown = dropdowns[i];
-            if (openDropdown.style.display === 'block') 
+            if (openDropdown.parentElement.classList.contains('show')) 
             {
-                openDropdown.style.display = 'none';
+                openDropdown.parentElement.classList.remove('show');
             }
         }
     }
